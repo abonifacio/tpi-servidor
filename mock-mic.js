38045ff0b86fe49947f04f3b86b3306eda4fb64e
Augusto Bonifacio <augusto.bonifacio95@gmail.com>
2017-10-08 18:20:46 -0300

    Incorporacion de microfono en windows

--- mock-mic.js
+++ mock-mic.js
@@ -1,13 +1,14 @@
 const dgram = require('dgram');
 const client = dgram.createSocket('udp4');
 const conf = require('./conf');
+const spawn = require('child_process').spawn;
 
 const HOST = conf.host;
 
 function start(PORT){
-	const spawn = require('child_process').spawn;
 
-	const rec = spawn('arecord',['-f','cd']);
+	// const rec = spawn('arecord',['-f','cd']); // linux
+	const rec = spawn('fmedia',['--record','-o','@stdout.wav']); //windows
 
 	rec.stdout.on('data',function(data){
 		client.send(data,0,data.byteLength,PORT,HOST,function(err,bytes){
